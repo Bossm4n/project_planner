04cefedebf9ed9539de3167ac463638a06f945f4
t06751bo <basil.osman@student.manchester.ac.uk>
2023-12-31 22:20:34 +0000

    Fully linked the projects and projects data to individual project pages

--- project-planner/src/app/projects/[projectid]/page.tsx
+++ project-planner/src/app/projects/[projectid]/page.tsx
@@ -1,66 +1,19 @@
 import Navbar from "@/app/common_components/navbar";
 import React from "react";
+import exampleProjects from "../projects";
 
-interface Project {
-  title: string;
-  description: string;
-  tasks_remaining: string[];
-}
-
-interface ExampleProjects {
-  [projectName: string]: Project;
-}
-
-const exampleProjects: ExampleProjects = {
-  project1: {
-    title: "ML for strawberry growth",
-    description: "Use machine learning to optimize strawberry growth.",
-    tasks_remaining: [
-      "Research existing ML models for plant growth.",
-      "Collect data on strawberry growth conditions.",
-      "Train a machine learning model.",
-      "Implement the model in a controlled environment.",
-    ],
-  },
-  project2: {
-    title: "Web Application Development",
-    description: "Build a modern web application for project management.",
-    tasks_remaining: [
-      "Design the user interface and user experience.",
-      "Implement the frontend using React.",
-      "Develop the backend using Node.js and Express.",
-      "Integrate with a database for data storage.",
-    ],
-  },
-  project3: {
-    title: "Mobile App for Fitness Tracking",
-    description: "Create a mobile app to track and monitor fitness activities.",
-    tasks_remaining: [
-      "Design the app interface for iOS and Android.",
-      "Implement features for tracking workouts and nutrition.",
-      "Integrate with health data APIs for additional insights.",
-      "Test the app on various devices and platforms.",
-    ],
-  },
-  project4: {
-    title: "Data Visualization Dashboard",
-    description: "Develop a dashboard for visualizing data analytics.",
-    tasks_remaining: [
-      "Design interactive charts and graphs.",
-      "Implement data visualization components using D3.js.",
-      "Connect the dashboard to a data source.",
-      "Optimize performance for large datasets.",
-    ],
-  },
-};
 const Project = ({ params }: { params: { projectid: string } }) => {
-  console.log(exampleProjects[params.projectid]);
   return (
     <div>
       <Navbar />
-      <div>{exampleProjects[params.projectid].title}</div>
-      <div>{exampleProjects[params.projectid].description}</div>
-      <ul>
+      <div className="mt-3 text-2xl">
+        {exampleProjects[params.projectid].title}
+      </div>
+      <div className="mt-1">
+        {exampleProjects[params.projectid].description}
+      </div>
+      <ul className="mt-2">
+        <div className="text-lg">Tasks Remaing:</div>
         {exampleProjects[params.projectid].tasks_remaining.map((task) => {
           return <li>{task}</li>;
         })}
--- project-planner/src/app/projects/page.tsx
+++ project-planner/src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../common_components/navbar";
 import Link from "next/link";
+import exampleProjects from "./projects";
 
 const Projects = () => {
   return (
@@ -16,27 +17,29 @@ const Projects = () => {
       <div className="flex flex-row mt-3 justify-around">
         {["Project_1", "Project_2", "Project_3", "Project_4"].map(
           (project, index) => {
+            const currProjectName = project.replace(/_/g, "").toLowerCase();
+
             return (
               <div className="flex flex-col items-center w-11/12">
                 {/* Project title and progress pie chart */}
-                <Link
-                  href={`projects/${project.replace(/_/g, "").toLowerCase()}`}
-                >
-                  {project}
-                </Link>
-                <div className="size-28 flex justify-center items-center border rounded-full mt-1 hover:transform hover:scale-105 hover:mt-1 transition-all">
-                  percent
+                <Link href={`projects/${currProjectName}`}>{project}</Link>
+                <div className="size-28 flex justify-center items-center border rounded-full mt-1 hover:transform hover:scale-105 hover:mt-1 transition-all text-xl">
+                  {exampleProjects[currProjectName].percent}
                 </div>
 
                 {/* Recent task */}
                 <div className="text-left w-11/12 border mt-2">
-                  <div>Most recent task</div>
+                  <div className="text-lg">
+                    <b>Most recent task:</b>
+                  </div>
                   <div>
-                    Magna eu deserunt reprehenderit sunt ullamco ea do esse
-                    duis.
+                    {exampleProjects[currProjectName].tasks_remaining[0]}
                   </div>
                   <div>Collaborators</div>
-                  <div>Projevt started: 12/12/12</div>
+                  <div>
+                    Date started:{" "}
+                    {exampleProjects[currProjectName].date_started}
+                  </div>
                 </div>
               </div>
             );
